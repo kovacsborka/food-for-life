f59942b2253ce7ade70c9e0e7a09f03e8629c843
kovacsborka <kovacsborbalaterez@gmail.com>
2022-02-08 17:56:29 +0100

    Co-authored-by: buikrisz00 <buikrisz00@users.noreply.github.com> Co-authored-by: Andras Varga <zoryandras@gmail.com> Co-authored-by: ElohegyiI <ElohegyiI@users.noreply.github.com>

---
+++ frontend/script.js
@@ -0,0 +1,48 @@
+function loadEvent(){
+
+    let img1 = document.getElementById('img1');
+    let img2 = document.getElementById('img2');
+    let img3 = document.getElementById('img3');
+    let imgArray = [img1, img2, img3];
+    let count = 1;
+
+    function changeImage(buttonId) {
+
+        for (const item of imgArray) {
+            item.classList.remove("showImg");
+            item.classList.add("noImg");
+        }
+        if (buttonId === "leftBtn") {
+            if (count < 1) {
+                imgArray[2].classList.remove("noImg");
+                imgArray[2].classList.add("showImg");
+                count = 2;
+            }
+            else {
+                imgArray[count].previousElementSibling.classList.remove("noImg");
+                imgArray[count].previousElementSibling.classList.add("showImg");
+                count--;
+            }
+        }
+        else {
+            if (count > 1) {
+                imgArray[0].classList.remove("noImg");
+                imgArray[0].classList.add("showImg");
+                count = 0;
+            }
+            else {
+                imgArray[count].nextElementSibling.classList.remove("noImg");
+                imgArray[count].nextElementSibling.classList.add("showImg");
+                count++;
+            }
+        } 
+     
+    }
+
+    document.getElementById('leftBtn').addEventListener("click", function(){ changeImage("leftBtn")});
+    document.getElementById('rightBtn').addEventListener("click", function(){ changeImage("rightBtn")});
+    
+  
+  };
+  
+  window.addEventListener('load', loadEvent);
